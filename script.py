35bed2cf41694a3774f1ac89263e917fd34b58f3
slopero <v.a.krasnov2005@mail.ru>
2026-04-14 00:10:52 +0700

    Добавил Dockerfile

--- script.py
+++ script.py
@@ -42,4 +42,4 @@ app.include_router(register_user.router, prefix="/users", tags=["users"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
